a1be05cf06e36b2aa1192386896914afd7c1c676
Luci2k26 <jo.ul.ah.he2k@gmail.com>
2026-07-13 18:13:07 -0400

    Primer commit - Proyecto VehiculosRest

---
+++ src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
@@ -0,0 +1,35 @@
+/*package com.vehiculos.VehiculosRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class VehiculosRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosRestApplication.class, args);
+	}
+}*/
+
+package com.vehiculos.VehiculosRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class VehiculosRestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VehiculosRestApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosRestApplication.class, args);
+	}
+}
