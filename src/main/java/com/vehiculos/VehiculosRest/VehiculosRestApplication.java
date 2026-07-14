652b2694bce38ccac89d98e4f818ae102ca4846a
Luci2k26 <jo.ul.ah.he2k@gmail.com>
2026-07-13 22:02:54 -0400

    prueba exit 1

--- src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
+++ src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
@@ -16,24 +16,5 @@ public class VehiculosRestApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(VehiculosRestApplication.class, args);
 	}
-}/*
-
-package com.vehiculos.VehiculosRest;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class VehiculosRestApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(VehiculosRestApplication.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(VehiculosRestApplication.class, args);
-	}
-}*/
+}
+//Prueba para push
