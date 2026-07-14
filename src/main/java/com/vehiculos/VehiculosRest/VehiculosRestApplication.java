22b6ecbdfb02909aae9eff3ac87a448addad3814
Luci2k26 <jo.ul.ah.he2k@gmail.com>
2026-07-13 21:43:54 -0400

    Intento 4

--- src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
+++ src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
@@ -1,18 +1,22 @@
-/*package com.vehiculos.VehiculosRest;
+package com.vehiculos.VehiculosRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 @SpringBootApplication
-public class VehiculosRestApplication {
+public class VehiculosRestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VehiculosRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VehiculosRestApplication.class, args);
 	}
-}*/
+}/*
 
 package com.vehiculos.VehiculosRest;
 
